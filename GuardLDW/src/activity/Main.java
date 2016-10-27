8d0f0bf49c1f6ae2645096c7b22f17d2c518072c
GuardLDW <303899225@qq.com>
2016-10-27 21:15:04 +0800

    添加了用户协议，在主界面调出菜单的打开用户协议选项后，通过WebView组件跳转到用户协议静态文本所属的URL，即可显示出用户协议

--- GuardLDW/src/activity/Main.java
+++ GuardLDW/src/activity/Main.java
@@ -9,6 +9,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -38,8 +41,8 @@ public class Main extends Activity{
 	public void onCreate(Bundle savedInstanceState){
 		
 		super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
         ActivityControl.addActivity(this);
         
@@ -144,6 +147,35 @@ public class Main extends Activity{
         });
         
 	}
+	
+	
+	public boolean onCreateOptionsMenu(Menu menu)  
+    {  
+        //MenuInflater class is used to instantiate menu XML files into Menu objects  
+        MenuInflater inflater = getMenuInflater();  
+        //将xml当做瘪的气球，inflate充气
+        inflater.inflate(R.menu.main, menu);  
+        return true;  
+    }  
+	
+	
+	public boolean onOptionsItemSelected(MenuItem item){  
+		
+        //得到当前选中的MenuItem的ID,  
+        int item_id = item.getItemId();  
+  
+        switch (item_id){
+        
+           case R.id.menu_license:
+        	   
+        	   Intent intent = new Intent(Main.this, License.class);
+        	   startActivity(intent);
+        	   break;
+        	   
+        	  
+        }  
+        return true;  
+    }  
 	
 	
     @Override
