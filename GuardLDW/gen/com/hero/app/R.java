ed89865fa534997515941eed00fc168de37a57db
GuardLDW <303899225@qq.com>
2016-11-24 22:02:11 +0800

    添加了Index界面，打开APP后先进入Index界面，短时间后自动进入主界面

--- GuardLDW/gen/com/hero/app/R.java
+++ GuardLDW/gen/com/hero/app/R.java
@@ -31,6 +31,7 @@ public final class R {
         public static final int game9=0x7f020011;
         public static final int hero=0x7f020012;
         public static final int ic_launcher=0x7f020013;
+        public static final int index=0x7f020014;
     }
     public static final class id {
         public static final int button_backtotitle=0x7f08000a;
@@ -90,13 +91,14 @@ public final class R {
         public static final int db=0x7f030000;
         public static final int game=0x7f030001;
         public static final int house=0x7f030002;
-        public static final int license=0x7f030003;
-        public static final int loadgame=0x7f030004;
-        public static final int login=0x7f030005;
-        public static final int main=0x7f030006;
-        public static final int register=0x7f030007;
-        public static final int set=0x7f030008;
-        public static final int special=0x7f030009;
+        public static final int index=0x7f030003;
+        public static final int license=0x7f030004;
+        public static final int loadgame=0x7f030005;
+        public static final int login=0x7f030006;
+        public static final int main=0x7f030007;
+        public static final int register=0x7f030008;
+        public static final int set=0x7f030009;
+        public static final int special=0x7f03000a;
     }
     public static final class menu {
         public static final int main=0x7f070000;
